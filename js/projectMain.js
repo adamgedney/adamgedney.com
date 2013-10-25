6ff4a452fd52cfd1f0c91bc4eda771b505df1038
Adam <adam.gedney@gmail.com>
2013-10-25 19:09:30 -0400

    Re-organized image directories and added projectsMain.js

---
+++ js/projectMain.js
@@ -0,0 +1,75 @@
+$(function(){
+
+	//contact icon hover states
+	//on mouse over
+	$(document).on('mouseover', 'img.github-icon', function(){
+		$(".github-icon").attr('src', 'images/github-icon-hover.png');
+	});
+
+	$(document).on('mouseover', 'img.forrst-icon', function(){
+		$(".forrst-icon").attr('src', 'images/forrst-icon-hover.png');
+	});
+
+	$(document).on('mouseover', 'img.linkedin-icon', function(){
+		$(".linkedin-icon").attr('src', 'images/linkedin-icon-hover.png');
+	});
+
+	$(document).on('mouseover', 'img.dribbble-icon', function(){
+		$(".dribbble-icon").attr('src', 'images/dribbble-icon-hover.png');
+	});
+
+	$(document).on('mouseover', 'img.fb-icon', function(){
+		$(".fb-icon").attr('src', 'images/fb-icon-hover.png');
+	});
+
+
+	//contact icon hover states
+	//on mouse out
+	$(document).on('mouseout', 'img.github-icon', function(){
+		$(".github-icon").attr('src', 'images/github-icon.png');
+	});
+
+	$(document).on('mouseout', 'img.forrst-icon', function(){
+		$(".forrst-icon").attr('src', 'images/forrst-icon.png');
+	});
+
+	$(document).on('mouseout', 'img.linkedin-icon', function(){
+		$(".linkedin-icon").attr('src', 'images/linkedin-icon.png');
+	});
+
+	$(document).on('mouseout', 'img.dribbble-icon', function(){
+		$(".dribbble-icon").attr('src', 'images/dribbble-icon.png');
+	});
+
+	$(document).on('mouseout', 'img.fb-icon', function(){
+		$(".fb-icon").attr('src', 'images/fb-icon.png');
+	});
+
+
+	//--------------------------------animate.css hover animations----------------------
+	$(document).on('mouseover', '.img-container a', function(){
+		$(this).addClass('animated pulse');
+	});
+
+	$(document).on('mouseout', '.img-container a', function(){
+		$(this).removeClass('animated pulse');
+	});
+
+	// profile pic swing
+	$(document).on('mouseover', 'img.profile-pic', function(){
+		$(this).addClass('animated swing');
+	});
+	//remove swing
+	$(document).on('mouseout', 'img.profile-pic', function(){
+		setTimeout(removeSwing,2500);		
+	});
+	function removeSwing(){
+		$('img.profile-pic').removeClass('animated swing');
+	};
+
+});// function
+
+
+
+
+
